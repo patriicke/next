2d892fb65cdaf7582f68808c41a9afb19556fddf
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-30 22:24:04 +0200

    I have shown that there is 404 or page is not found in nextjs

---
+++ pages/404/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <Head>
+        <title>Page not found</title>
+      </Head>
+      <h1>404 Page not found</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
