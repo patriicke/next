f49cc21870eaf3a4526d5154a044a4a4418ec614
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-30 21:33:17 +0200

    I have also intergrated the link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <Link href="about">About</Link>
       <h1>Hello World</h1>
     </div>
   );
